a27ebd0949198410fc9548bd48dfe863c20eb4fb
sumitvish20 <sumitvds360@gmail.com>
2023-11-17 18:09:47 +0530

    Build a Responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Show } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <>
+      <Grid templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`
+      }}>
+        <GridItem pl="2" bg="pink.300" area={"nav"}>
+          Nav
+        </GridItem>
+        <GridItem pl="2" bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+        <Show above="lg">
+        <GridItem pl="2" bg="blue.300" area={"aside"}>
+          Aside
+        </GridItem>
+        </Show>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
