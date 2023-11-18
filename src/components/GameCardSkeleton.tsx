f0c5bf28c4193fd1c402b83f056053b834090058
sumitvish20 <sumitvds360@gmail.com>
2023-11-18 19:52:20 +0530

    Refractoring styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -4,7 +4,7 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
 const GameCardSkeleton = () => {
   //mimiking same structure As our gameCard looks like
   return (
-    <Card width='250px' borderRadius={10} overflow='hidden'>
+    <Card >
        <Skeleton height='200px'/>
        <CardBody>
         <SkeletonText/>
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,22 +3,32 @@ import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  
-  const {error, games, isLoading} =  useGames();
+  const { error, games, isLoading } = useGames();
 
   //Array for showing skeletons
-  const skeletons = [1,2,3,4,5,6,7,8,9,10];
-
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={10} padding={10}>
-        {isLoading && skeletons.map((skeleton) => <GameCardSkeleton key={skeleton}/>)}
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding={10}
+      >
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game} />
+          <GameCardContainer>
+            <GameCard key={game.id} game={game} />
+          </GameCardContainer>
         ))}
       </SimpleGrid>
     </>
