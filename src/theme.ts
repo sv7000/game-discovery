d6738f9036abeed7808bf838a0c56a00700047ec
sumitvish20 <sumitvds360@gmail.com>
2023-11-21 17:51:24 +0530

    Theme customize

--- src/theme.ts
+++ src/theme.ts
@@ -9,6 +9,26 @@ const config: ThemeConfig = {
 }
 
 // 3. extend the theme
-const theme = extendTheme({ config })
+const theme = extendTheme({ config,
+  color: {
+    gray:
+
+  
+{
+  50: '#ffe2ec',
+  100: '#ffb3c5',
+  200: '#fc839f',
+  300: '#f95278',
+  400: '#f62252',
+  500: '#dd0939',
+  600: '#ad032c',
+  700: '#7c001e',
+  800: '#4d0012',
+  900: '#200005',
+}
+
+  }
+})
 
 export default theme;
+
