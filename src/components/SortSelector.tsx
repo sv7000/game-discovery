bfdb26a74c7949b41ce928ca0fdd6f15b6d32802
sumitvish20 <sumitvds360@gmail.com>
2023-11-21 13:35:30 +0530

    Added Sort  Functionality

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,14 +2,29 @@ import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
 import { BsChevronBarDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props{
+  onSelectSortOrder:  (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }:Props) => {
+
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(order => order.value === sortOrder);
+
   return (
     <Menu>
-      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>Order By: Relevance</MenuButton> 
+      <MenuButton as={Button} rightIcon={<BsChevronBarDown />}>Order By: {currentSortOrder?.label || 'Relevance'}</MenuButton> 
       <MenuList>
-        <MenuItem>Item 1</MenuItem>
-        <MenuItem>Item 2</MenuItem>
-        <MenuItem>Item 3</MenuItem>
+        {sortOrders.map(order => <MenuItem onClick={() => onSelectSortOrder(order.value)} value={order.value} key={order.value}>{order.label}</MenuItem>)}
       </MenuList>
     </Menu>
   );
--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,8 +1,9 @@
-
+import NoImg from '../assets/no-image-placeholder.webp'
 
 const getCroppedImageUrl = (url: string) => {
+  if(!url) return NoImg;
  const index = url.indexOf('media/') + 'media/'.length;
  return url.slice(0, index) + 'crop/600/400/' + url.slice(index);
 }
 
-export default getCroppedImageUrl
+export default getCroppedImageUrl;
