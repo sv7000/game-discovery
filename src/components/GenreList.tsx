ae653383a4fbd194782ed57ac98f1c9bcbfa6822
sumitvish20 <sumitvds360@gmail.com>
2023-11-21 17:13:00 +0530

    Cleanup genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,4 +1,4 @@
-import { Button, HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { Button, HStack, Heading, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenre, { Genre } from "../hooks/useGenre";
 import GenreSkeleton from "./GenreSkeleton";
 
@@ -14,18 +14,21 @@ const GenreList = ({ selectedGenre,onSelectedGenre }: Props ) => {
   // const {genres} = useGenre();
   const skeletonsList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
   return (
+    <>
+    <Heading fontSize='2xl' marginBottom={3}>Genres</Heading>
     <List>
       {isLoading && skeletonsList.map((skeleton)=> <GenreSkeleton key={skeleton}/>)}
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY={2}>
           <HStack>
-            <Image boxSize='32px' borderRadius={8} src={genre.image_background} />
-            <Button fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} fontSize='lg' variant='link'onClick={() => onSelectedGenre(genre)} >{genre.name}</Button>
+            <Image boxSize='32px' objectFit='cover' borderRadius={8} src={genre.image_background} />
+            <Button whiteSpace='normal' textAlign='left' fontWeight={genre.id === selectedGenre?.id ? 'bold' : 'normal'} fontSize='lg' variant='link'onClick={() => onSelectedGenre(genre)} >{genre.name}</Button>
           </HStack>
           
         </ListItem>
       ))}
     </List>
+    </>
   );
 };
 
